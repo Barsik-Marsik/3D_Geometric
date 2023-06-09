4c4fe15efa457d021ea61ebcb7a2aa0225499468
Barsik-Marsik <zorinilya23@gmail.com>
2023-06-09 21:51:32 +0300

    Add return first derivative for circle per parameter t
    - Add "GetDerivative" method;
    - Add "GetDerivateOfCurve" function;

--- src/3D_Geometric.cpp
+++ src/3D_Geometric.cpp
@@ -8,11 +8,13 @@
 
 #include <cmath>
 #include <iostream>
+#include <limits>
 //#include <numbers>
 
 
 #define EPS 1.0e-15
 
+constexpr double INF = std::numeric_limits<double>::infinity();
 constexpr double pi = std::acos(-1);
 // constexpr double pi = std::numbers::pi;
 
@@ -70,7 +72,7 @@ public:
 
 	virtual Point GetPoint(double t) const {};
 
-//	virtual double GetDerivative(double t) const;
+	virtual double GetDerivative(double t) const {};
 
 	CurveType GetType() const {
 		return type_;
@@ -115,9 +117,36 @@ public:
 		return center_ + p;
 	}
 
-	double GetDerivative(double t) const {
-		double value = 0.0;
-		return value;
+	// The derivate at a point is the tangent of the slope of tangent line
+	// at that point. Equation of a tangent line: Ax + By + C = 0
+	// Vector R{A; B} from the center of the circle to the point p
+	virtual double GetDerivative(double t) const {
+		Point p = GetPoint(t);
+		double A = p.X - center_.X;
+		double B = p.Y - center_.Y;
+		double C = -A * p.X - B * p.Y;
+		// If B == 0, the tangent line parallel to Y axis
+		if (std::abs(B) < EPS) {
+			return INF;
+		}
+		// If A == 0, the tangent line parallel to X axis
+		if (std::abs(A) < EPS) {
+			return 0.0;
+		}
+		// If C != 0, the tangent line cuts off segments a and b on the X and Y axes
+		// tang = b/a
+		if (std::abs(C) > EPS) {
+			double a = -C / A;
+			double b = -C / B;
+			if ((a > 0 && b > 0) || (a < 0 && b < 0)) {
+				return -b / a;
+			}
+			return b / a;
+		}
+		// If C == 0, the tangent line passes through the point {0; 0}
+		// Equation: Ax + By = 0 => y = -Ax / B
+		// tang = -A / B
+		return -A / B;
 	}
 
 private:
@@ -125,10 +154,9 @@ private:
 	double radius_;
 };
 
-
 template<typename Curve>
-void GetDerivateOfCurve(const Curve& curve) {
-	std::cout << "GetPoint: " << curve.GetPoint(pi) << std::endl;
+void GetDerivateOfCurve(const Curve& curve, double t) {
+	std::cout << "GetDerivate: " << curve.GetDerivative(t) << std::endl;
 }
 
 int main() {
@@ -140,7 +168,7 @@ int main() {
 	std::cout << "R=5, (5.0; 5.0), pi: " << circle_2.GetPoint(pi/6) << std::endl;
 
 	Curve curve_2(CurveType::Circle);
-	GetDerivateOfCurve(circle_2);
+	GetDerivateOfCurve(circle_2, pi/4);
 
 	return 0;
 }
